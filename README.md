45b0d038929348646fe06181e23ee361beb3b8bc
Keyve Alves De Oliveira <keyve_alves@hotmail.com>
2023-10-21 01:03:11 -0300

    Adicionando projeto ao git
    Realizando o primeiro commit inserindo o projeto ao github.

---
+++ README.md
@@ -0,0 +1,2 @@
+# dio_flutter_finalproject
+ Este projeto faz parte do desafio de código do bootcamp de flutter da plataforma de ensino DIO.
---
+++ android/app/src/main/kotlin/com/keyve/finalproject/dio_flutter_finalproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.keyve.finalproject.dio_flutter_finalproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
